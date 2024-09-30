803f49775d2cc7d7b9b0516cd5180a71326e8a2d
Sourav-students <saurabhsinghjpnagar8223840622@gmail.com>
2024-09-30 23:58:44 +0530

    Add files via upload

---
+++ divison.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main ()
+{
+    int a,b,div;
+
+printf("Enter the two value");
+scanf("%d%d",&a,&b);
+div=a/b;
+printf("Result of sub is%d",&div);
+return 0;
+}
---
+++ multi.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main ()
+{
+    int a,b,multi;
+
+printf("Enter the two value");
+scanf("%d%d",&a,&b);
+multi=a*b;
+printf("Result of sub is%d",&multi);
+return 0;
+}
