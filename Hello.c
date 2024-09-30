d4682f6b0e5618549e40d037370eebe3e4e39b24
Sourav-students <saurabhsinghjpnagar8223840622@gmail.com>
2024-09-30 23:47:05 +0530

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+
+int main() 
+{
+  int a,b,add;
+  
+  printf("Enter the value of a&b");
+
+  scanf("%d%d",&a,&b);
+  add=a+b;
+  printf("result of Add is%d",&add);
+
+  return 0; }
+  
