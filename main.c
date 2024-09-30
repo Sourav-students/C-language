a3cec811778c01ddd3552fe81fa58c0a7b1c0364
Sourav-students <saurabhsinghjpnagar8223840622@gmail.com>
2024-09-30 23:06:05 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+    {
+       int add();
+ 
+        int a,b,c;
+        printf("enetr the 2 value");
+        scanf("%d%d",&a,&b);
+        c=a+b;
+        printf("%d",&c);
+
+        return 0;
+    }
