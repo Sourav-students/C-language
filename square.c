bf340b8714d24b64e07bfda442ea513dbed36e78
Sourav-students <saurabhsinghjpnagar8223840622@gmail.com>
2024-10-01 11:57:42 +0530

    Update square.c

--- square.c
+++ square.c
@@ -3,11 +3,11 @@
 int main()
 {
     int r,squ;
-    printf("Enter the value of &r");
+    printf("Enter the value of r");
     scanf("%d",&r);
 
     squ=r*r;
-    printf("area of square is %d/n",&squ);
+    printf("area of square is %d/n",squ);
 
     return 0;
 
