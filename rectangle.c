8683a4796acf0d4d8d9faf70c17793b6e5ab82ae
Sourav-students <saurabhsinghjpnagar8223840622@gmail.com>
2024-10-01 11:55:22 +0530

    Update rectangle.c

--- rectangle.c
+++ rectangle.c
@@ -3,11 +3,11 @@
 int main()
 {
     int l,b,rec;
-    printf("Enter the value of l&b");
+    printf("Enter the value of l and b");
     scanf("%d%d",&l,&b);
 
     rec=l*b;
-    printf("area of rectangle is %d/n",&rec);
+    printf("area of rectangle is %d/n",rec);
 
     return 0;
 
