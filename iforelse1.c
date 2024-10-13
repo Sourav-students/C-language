4a5a22863ee31aa6caddc5c711f32b0dc0886f13
Sourav singh <saurabhsinghjpnagar8223840622@gmail.com>
2024-10-13 22:14:04 +0530

    Add files via upload

---
+++ iforelse1.c
@@ -0,0 +1,16 @@
+//check if number is postive else negative
+#include<stdio.h>
+
+int main(){
+    int Num;
+    printf("check the numbar is pos/neg");
+    scanf("%d",&Num);
+
+    if (Num >= 0, Num <= 0){
+        printf("%d is negative.\n",Num);
+    }
+    else
+    printf("%d is postive.\n",Num);
+
+    return 0;
+}
