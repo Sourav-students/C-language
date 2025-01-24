0c734ed5056ef26e8dc6446f4da005915f6ec9d3
Sourav singh <saurabhsinghjpnagar8223840622@gmail.com>
2025-01-24 20:34:52 +0530

    Add files via upload

---
+++ arithmeticoperators.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main ()
+//Arthematic operators
+{
+    int a,b,multi,Add,Sub,mod;
+    float Div;
+
+printf("Enter the two value");
+scanf("%d%d",&a,&b);
+multi=a*b;
+printf("Result of multi is %d\n",multi);
+
+Add=a+b;
+printf("result of Add is %d\n",Add);
+
+Sub=a-b;
+printf("result of Sub is %d\n",Sub);
+
+mod=a%b;
+printf("result of mod is %d\n",mod);
+
+Div=a/b;
+printf("result of Div is %f\n",Div);
+return 0;
+}
