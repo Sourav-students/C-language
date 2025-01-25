e6c0df5fabf450ec9ae493f40bcf6354d2e4a0d4
Sourav singh <saurabhsinghjpnagar8223840622@gmail.com>
2025-01-25 13:00:43 +0530

    Add files via upload

---
+++ relational_operator.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+
+int main () {
+
+    int x,y;
+    printf("enter two value\n");
+    scanf("%d%d",&x,&y);
+
+  //  relational operators equal(==), notequal(!=), greater then(>), less then(<),greater then equal(>=), less then equal(<=)
+
+  if(x==y){
+    printf("true\n");
+  }
+  else{
+    printf("false\n");
+  }
+
+ if(x!=y){
+    printf("true\n");
+  }
+  else{
+    printf("false\n");
+  }
+
+ if(x>y){
+    printf("true\n");
+  }
+  else{
+    printf("false\n");
+  }
+
+ if(x<y){
+    printf("true\n");
+  }
+  else{
+    printf("false\n");
+  }
+
+
+ if(x>=y){
+    printf("true\n");
+  }
+  else{
+    printf("false\n");
+  }
+
+ if(x<=y){
+    printf("true\n");
+  }
+  else{
+    printf("false\n");
+  }
+
+  return 0;
+
+
+
+}
